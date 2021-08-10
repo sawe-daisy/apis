44c70fe3af820ba193a9597d84eec3a7c4cd6450
sawe-daisy <deesawe16@gmail.com>
2021-08-10 18:52:14 +0300

    adding user model

--- apiS/settings.py
+++ apiS/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jobApis',
-    'rest_framework'
+    'rest_framework',
+    'phonenumber_field'
 ]
 
 MIDDLEWARE = [
--- jobApis/models.py
+++ jobApis/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
+from django.db.models import CharField, IntegerField
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.translation import ugettext as _
 
 # Create your models here.
+
+USER_TYPE = (
+    ('JS',"Job Seeker"),
+    ('EM',"Employer")
+)
+
+class User(AbstractUser):
+    firstname = CharField(_("Firstname"),blank=True, null=True,max_length=255)
+    lastname = CharField(_("Lastname"),blank=True, null=True,max_length=255)
+    username = CharField(_("Username"), null=True,max_length=255, unique=False)
+    email = CharField(_("Email"),blank=True, null=True,max_length=255)
+    phone_no = PhoneNumberField(_("Phone Number"),blank=True,null=True)
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+
+    def __str__(self) -> str:
+        return self.username
+   
+    
+    def get_absolute_url(self):
+        """Get url for user's detail view.
+
+        Returns:
+            str: URL for user detail.
+
+        """
+        return reverse("users:detail", kwargs={"username": self.username})
+
+    @property
+    def is_jobseeker(self):
+        if str(self.user_type) == "JS":
+            return True
+        return False
+
+    @property
+    def is_employer(self):
+        if str(self.user_type) == "EM":
+            return True
+        return False
+
+    def  get_full_name(self):
+        return self.firstname + ' ' + self.lastname
